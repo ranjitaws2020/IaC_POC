439cd924afe135ef71f1bf46fef88dc0aa687433
Arti1998 <38453172+Arti1998@users.noreply.github.com>
2023-02-02 19:42:06 +0530

    Update HostOnIIS.groovy

--- Jenkins-CICD-Pipeline/stages/HostOnIIS.groovy
+++ Jenkins-CICD-Pipeline/stages/HostOnIIS.groovy
@@ -1,1 +1,19 @@
-
+def call()
+{
+  try {
+                        echo "Stopping App Pool"
+                        bat "%systemroot%\\system32\\inetsrv\\appcmd stop apppool /apppool.name:DefaultAppPool"
+                    }
+                    catch (Exception err) {
+                        echo err.getMessage()
+                    }
+                    
+                    echo "Cleaning the app folder"
+                    bat "del \"C:\\inetpub\\wwwroot\\UploadDowFilesJenkinsPipeline\\**\" /S /Q"
+                    
+                    echo "Copying the published files to the app folder"
+                    bat "xcopy \"C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\DotNetAPI-CICD\\DotNetAPI_CI\\bin\\publish\\**\" \"C:/inetpub/wwwroot/UploadDowFilesJenkinsPipeline\" /E /Q"
+                    
+                    echo "Starting App Pool"
+                    bat "%systemroot%\\system32\\inetsrv\\appcmd start apppool /apppool.name:DefaultAppPool"
+}
