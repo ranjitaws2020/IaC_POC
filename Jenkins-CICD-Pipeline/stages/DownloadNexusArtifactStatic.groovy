ec5256fc6607d8a3244b9db5349ba00b16aba176
Arti1998 <38453172+Arti1998@users.noreply.github.com>
2023-02-02 19:36:22 +0530

    Update DownloadNexusArtifactStatic.groovy

--- Jenkins-CICD-Pipeline/stages/DownloadNexusArtifactStatic.groovy
+++ Jenkins-CICD-Pipeline/stages/DownloadNexusArtifactStatic.groovy
@@ -1,1 +1,6 @@
-
+def call()
+{
+   withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'nexus-user', passwordVariable: 'pw', usernameVariable: 'user']]) {
+                    bat(script: 'curl -L -XGET "http://localhost:8081/repository/dotnet-repo/FileOperation%2F1.0.0" --output FileOperation.nupkg -u %user%:%pw%')
+                  }
+}
