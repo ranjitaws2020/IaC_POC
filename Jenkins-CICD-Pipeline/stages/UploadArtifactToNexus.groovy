4cfb4a494e15ce9aad44154a65b4ccb52b3c2bac
Arti1998 <38453172+Arti1998@users.noreply.github.com>
2023-02-02 19:33:59 +0530

    Update UploadArtifactToNexus.groovy

--- Jenkins-CICD-Pipeline/stages/UploadArtifactToNexus.groovy
+++ Jenkins-CICD-Pipeline/stages/UploadArtifactToNexus.groovy
@@ -1,1 +1,5 @@
-
+def call()
+{
+   bat 'dotnet pack .\\UploadandDownloadFiles\\UploadandDownloadFiles.csproj'
+   bat 'dotnet nuget push .\\UploadandDownloadFiles\\bin\\Debug\\FileOperation.1.0.0.nupkg -k 2bcfcfe0-5b00-38e1-9086-b9e67205f16d -s http://localhost:8081/repository/dotnet-repo/'
+}
