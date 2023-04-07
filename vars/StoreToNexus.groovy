25a03179e4b8873b8975d1917da9e65a53759023
ranjitaws2020 <77003552+ranjitaws2020@users.noreply.github.com>
2023-04-07 20:21:13 +0530

    Update StoreToNexus.groovy

--- vars/StoreToNexus.groovy
+++ vars/StoreToNexus.groovy
@@ -1,4 +1,4 @@
 def call()
 {
-   bat label: '', script: 'C:\\Ranjit\\Code Samples\\apache-maven-3.9.1\\bin\\mvn.cmd clean deploy'
+   bat label: '', script: 'C:\\build-tools\\apache-maven-3.9.1\\bin\\mvn.cmd clean deploy'
 }
