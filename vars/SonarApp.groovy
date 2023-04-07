2a6c8e73ac7a10384e3fea658843b51819019d75
ranjitaws2020 <77003552+ranjitaws2020@users.noreply.github.com>
2023-04-07 20:07:13 +0530

    Update SonarApp.groovy

--- vars/SonarApp.groovy
+++ vars/SonarApp.groovy
@@ -1,5 +1,5 @@
 def call(login)
 {
-//bat label: '', script: """E:\\Devops\\apache-maven-3.8.6\\bin\\mvn.cmd clean verify sonar:sonar -Dsonar.host.url=http://localhost:9000 -Dsonar.login=${login}"""
+ 
   bat label: '', script: """C:\\Ranjit\\Code Samples\\apache-maven-3.9.1\\bin\\mvn.cmd clean verify sonar:sonar -Dsonar.host.url=http://localhost:9000 -Dsonar.login=${login}"""
 }
