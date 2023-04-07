1cdfc8940961d78de4a9170f0f2ef74694e17954
ranjitaws2020 <77003552+ranjitaws2020@users.noreply.github.com>
2023-04-07 19:49:26 +0530

    Update BuildApp.groovy

--- vars/BuildApp.groovy
+++ vars/BuildApp.groovy
@@ -1,4 +1,5 @@
 def call()
 {
-bat label: '', script: 'E:\\Devops\\apache-maven-3.8.6\\bin\\mvn.cmd clean package'  
+//bat label: '', script: 'E:\\Devops\\apache-maven-3.8.6\\bin\\mvn.cmd clean package'  
+  bat label: '', script: 'C:\\Ranjit\\Code Samples\\apache-maven-3.9.1\\bin\\mvn.cmd clean package'  
 }
